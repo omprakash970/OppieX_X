ecc66099485fa86c6395ceba00370f4f6b830f88
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-10 23:51:46 +0530

    done

--- frontend/src/landing_page/Home/Pricing.jsx
+++ frontend/src/landing_page/Home/Pricing.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+
 function Pricing() {
-  return <h1>Pricing Page</h1>;
+ return(
+    <>
+    <div className="container">
+        <div className="row">
+            <div className="col-4">
+                <h1 className="mb-3">UnBeatable Pricing</h1>
+                <p></p>
+            </div>
+            <div className="col-2"></div>
+            <div className="col-6"></div>
+        </div>
+    </div>
+    </>
+ );
 }
 export default Pricing;
