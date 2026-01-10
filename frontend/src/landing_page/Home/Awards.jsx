94df4cc5c415227e2b554941f54891229d27b124
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-10 23:03:31 +0530

    done

--- frontend/src/landing_page/Home/Awards.jsx
+++ frontend/src/landing_page/Home/Awards.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 function Awards() {
     return (
-        <h1>Awards Page</h1>
+        <div className="container" mt-5>
+            <div className="row">
+                <div className="col-6">
+                    <img src="./public/Media/largestBroker.svg" alt="Awards Image" className="mb-5" />
+                </div>
+                <div className="col-6">
+                    <h1>Largest Stock Broker in India</h1>
+                    <p>OppieX is recognized as the largest stock broker in India, providing unparalleled trading services to millions of users.
+                    </p>
+                    <ul>
+                        <li>
+                            <p></p>
+                        </li>
+                        <li>
+                            <p></p>
+                        </li>
+                        <li>
+                            <p></p>
+                        </li>
+                        <li>
+                            <p></p>
+                        </li>
+                        <li>
+                            <p></p>
+                        </li>
+                        <li>
+                            <p></p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
     );
 }   
 export default Awards;
--- frontend/src/landing_page/Home/Hero.jsx
+++ frontend/src/landing_page/Home/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 function Hero() {
     return (
-      <div className="container p-5">
+      <div className="container p-5" mb-5>
         <div className="row text-center">
            <img src="./public/Media/homeHero.png" alt="Hero Image" className="mb-5" ></img>
            <h1 className="mt-5" >
@@ -9,9 +9,9 @@ function Hero() {
            </h1>
            <p>Mine&gt;&gt;&gt;&gt;&gt;&gt;&gt;&gt;&gt;&gt;
 Zerodha 😇</p>
-            <button className="btn btn-primary fs-5 mt-3" style={
+            <button className="btn btn-primary fs-5 mt-3 mb-5" style={
                 {width: "25%", margin:"0 auto", borderRadius: "15px", padding:"5px", color:"white"}
-              }>
+              } >
                 SignUp
             </button>
         </div>
