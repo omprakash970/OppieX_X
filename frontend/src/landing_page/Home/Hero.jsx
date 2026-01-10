e8404761f5f3e25c8f488ef2ecf2425ab8842f68
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-10 22:03:45 +0530

    done with the hero

--- frontend/src/landing_page/Home/Hero.jsx
+++ frontend/src/landing_page/Home/Hero.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 function Hero() {
     return (
-        <h1>Hero Section</h1>
+      <div className="container">
+        <div className="row">
+            <div className="col">
+
+            </div>
+             <div className="col">
+
+            </div>
+             <div className="col">
+
+            </div>
+        </div>
+      </div>
     );
 }
 export default Hero;
