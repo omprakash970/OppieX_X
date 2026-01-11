6f526d15cd11544b7103b658b707b8877d38c33a
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-11 15:11:51 +0530

    fixed routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/fonts.css";
 import "./styles/responsive.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-
+ <StrictMode>
       <App />
   </StrictMode>
 );
